fdb6d8da528834b7edf79e8f48f2308275b23617
keyoni <56132773+keyoni@users.noreply.github.com>
2020-10-07 15:40:14 -0700

    added logic to button
    now converts from F to C and C to F

--- app/src/main/java/com/example/temperaturecalculator/MainActivity.kt
+++ app/src/main/java/com/example/temperaturecalculator/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.temperaturecalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import kotlinx.android.synthetic.main.activity_main.*
+import java.math.RoundingMode
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var temperatureConvert = ""
+        spOptions.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
+
+            override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                temperatureConvert = adapterView?.getItemAtPosition(position).toString()
+            }
+
+        }
+        btnConvert.setOnClickListener {
+            var userTemperature = etTemperature.text.toString().toDouble()
+            var finalTemp: Double;
+            if(temperatureConvert == "F -> C") {
+                finalTemp = ((userTemperature - 32) * (5.0/9.0))
+            }else {
+                finalTemp = ((userTemperature * (9.0/5.0) + 32))
+            }
+            val df = DecimalFormat("#.##")
+            df.roundingMode = RoundingMode.CEILING
+            finalTemp = df.format(finalTemp).toDouble()
+            tvResult.text = finalTemp.toString()
+        }
     }
+
 }
